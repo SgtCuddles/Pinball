4e43bdd82a1fc03dcde8bacc44f8d3c4adca9220
Zachary Daly <zdaly3183@gmail.com>
2019-01-07 13:08:40 -0800

    Starting targets

---
+++ Pinball/Assets/Scripts/BallScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public GameObject Ball;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "BallIgnore")
+        {
+            Physics.IgnoreCollision(collision.collider, Ball.GetComponent<Collider>());
+        }
+    }
+}
--- Pinball/Assets/Scripts/PlungerScript.cs
+++ Pinball/Assets/Scripts/PlungerScript.cs
@@ -4,13 +4,65 @@ using UnityEngine;
 
 public class PlungerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    public GameObject   plunger;
+    Rigidbody           rb;
+    double              initPosition;
+    double              position;
+
+    public bool         key;
+    public string       keyName;
+    public int          speed;
+    Vector3             velocity;
+    Vector3             nVelocity;
+
+
+
+    void Start ()
+    {
+        rb = plunger.GetComponent<Rigidbody>();
+        initPosition = rb.position.z;
+        velocity = new Vector3(0, 0, speed);
+        nVelocity = new Vector3(0, 0, speed/4 * -1);
+
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if(Input.GetKey(keyName))
+        {
+            key = true;
+        }
+        else
+        {
+            key = false;
+        }
+        position = rb.position.z;
+        
 	}
+
+    private void FixedUpdate()
+    {
+        if(key)
+        {
+            rb.AddForce(nVelocity);
+        }
+        else if(initPosition >= position)
+        {
+            rb.AddForce(velocity);
+        }
+        else if(initPosition <= position && initPosition != position)
+        {
+            rb.AddForce(nVelocity);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "plungerStop")
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
 }
